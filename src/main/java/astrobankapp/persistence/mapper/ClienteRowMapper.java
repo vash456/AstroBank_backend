f07f3ae7457c5f394ed4a98717c46b2f7f500242
Carolina Bedoya Rendón <aonixsoul@gmail.com>
2026-05-18 18:09:13 -0500

    fix: creación db clientes

--- src/main/java/astrobankapp/persistence/mapper/ClienteRowMapper.java
+++ src/main/java/astrobankapp/persistence/mapper/ClienteRowMapper.java
@@ -13,13 +13,14 @@ public class ClienteRowMapper implements RowMapper<Cliente>{
         Cliente cliente = new Cliente();
 
         cliente.setId(rs.getInt("id"));
-        cliente.setIdentificacion(rs.getString( "identificacion");
-        cliente.setNombreCompleto(rs.getString ("nombre_completo");
-        cliente.setCelular(rs.getString ("celular");
-        cliente.setUsuario(rs.getString ("usuario");
-        cliente.setContrasena(rs.getString ("contrasena_hash");
-        cliente.setIntentosFallidos(rs.getInt ("intentos_fallidos");
-        cliente.setBloqueado(rs.getInt ("bloqueado");
+        cliente.setIdentificacion(rs.getString( "identificacion"));
+        cliente.setNombreCompleto(rs.getString ("nombre_completo"));
+        cliente.setCelular(rs.getString ("celular"));
+        cliente.setUsuario(rs.getString ("usuario"));
+        cliente.setContrasena(rs.getString ("contrasena_hash"));
+        cliente.setIntentosFallidos(rs.getInt ("intentos_fallidos"));
+        cliente.setBloqueado(rs.getBoolean("bloqueado"));
+
 
         return cliente;
 
--- src/main/java/astrobankapp/persistence/repository/ClienteRepositoryAdapterMySql.java
+++ src/main/java/astrobankapp/persistence/repository/ClienteRepositoryAdapterMySql.java
@@ -20,8 +20,34 @@ public class ClienteRepositoryAdapterMySql implements ClientePersistencePort {
 
     @Override
     public Cliente saveCliente(Cliente cliente) {
-        return null;
+       String sql = "INSERT INTO cliente " +
+               "(id, identificacion, nombre_completo,celular, usuario, contrasena_hash, intentos fallidos, bloqueado)" +
+               "VALUES (?,?,?,?,?,?,?,?)";
+        try(PreparedStatement ps = connection.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS)){
+            setClienteParams(ps, cliente);
+            ps.executeUpdate();
+            ResultSet keys = ps.getGeneratedKeys();
+            if (keys.next()){
+                cliente.setId(keys.getInt(1));
+            }
+        }catch (SQLException e){
+            throw new RuntimeException("Error al guardar cliente", e);
+        }
+        return cliente;
     }
+
+    private void setClienteParams(PreparedStatement ps, Cliente cliente) throws SQLException{
+        ps.setInt(1, cliente.getId());
+        ps.setString(2, cliente.getIdentificacion());
+        ps.setString(3, cliente.getNombreCompleto());
+        ps.setString(4,cliente.getCelular());
+        ps.setString(5, cliente.getUsuario());
+        ps.setString(6, cliente.getContrasena());
+        ps.setInt(7, cliente.getIntentosFallidos());
+        ps.setBoolean(8, cliente.isBloqueado());
+
+    }
+
 
     @Override
     public List<Cliente> findAllClientes() {
@@ -35,6 +61,7 @@ public class ClienteRepositoryAdapterMySql implements ClientePersistencePort {
         }catch (SQLException e){
             throw new RuntimeException("Error al buscar los datos: " + e);
         }
+        return clientes;
     }
 
     @Override
